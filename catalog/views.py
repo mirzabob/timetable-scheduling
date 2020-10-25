7a91466101920769a73da8f0d04ec457f794bc7a
Nitish Bharti <nitishbharti235@gmail.com>
2020-10-26 01:29:22 +0530

    modify code to generate time table by taking input directly from HTML page

--- catalog/views.py
+++ catalog/views.py
@@ -10,6 +10,9 @@ from django.views.decorators.csrf import csrf_exempt
 # importing scheduler class
 from .Scheduler import Scheduler
 
+# importing custom forms
+from .forms import Input
+
 # importing models
 from .models import Course, Classroom, Lecturer, Class, StudentGroup
 
@@ -22,34 +25,27 @@ def get_random_lecturer(lecturers):
 
 @csrf_exempt
 def schedule(request):
-    if request.method == 'GET':
-        print('hello')
-        print(request)
-        return HttpResponse("Hello World!")
-
     if request.method == 'POST':
-        data = json.loads(request.body)
-        classrooms = data["classroom"]
-        lecturers = data["lecturer"]
-        courses = data["course"]
-        student_groups = data["studentgroup"]
+        input_data = Input(request.POST)
+        if input_data.is_valid():
+            courses, lecturers, classrooms, student_groups = input_data.clean_jsonfield()
 
-        class_groups = []
+            class_groups = []
 
-        for student_group in student_groups:
-            group = student_group["name"]
-            group_courses = student_group["courses"]
-            for course in group_courses:
-                random_lecturer = get_random_lecturer(lecturers)
-                class_group = [group, course, random_lecturer, 6]
-                class_groups.append(class_group)
+            for student_group in student_groups:
+                group = student_group["name"]
+                group_courses = student_group["courses"]
+                for course in group_courses:
+                    random_lecturer = get_random_lecturer(lecturers)
+                    class_group = [group, course, random_lecturer, 6]
+                    class_groups.append(class_group)
 
-        rooms = []
-        for classroom in classrooms:
-            rooms.append(classroom["name"])
+            rooms = []
+            for classroom in classrooms:
+                rooms.append(classroom["name"])
 
-        schedule_t = Scheduler(rooms, class_groups)
-        schedule_t.find_fittest()
+            schedule_t = Scheduler(rooms, class_groups)
+            schedule_t.find_fittest()
 
-        print(schedule_t.timeTable)
-        return HttpResponse('Recieved the data and timetable generated')
+            print(schedule_t.timeTable)
+    return render(request, 'index.html')
